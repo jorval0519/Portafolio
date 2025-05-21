7afa19261dde1d2372bda23cd41a2b9cfba2f5d9
jorval0519 <185670@upslp.edu.mx>
2025-05-21 13:03:37 -0600

    Add files via upload

---
+++ Operaciones con JavaScript.js
@@ -0,0 +1,31 @@
+function sumar() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const resultado = num1 + num2;
+    document.getElementById("r").textContent = resultado;
+}
+
+function restar() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const resultado = num1 - num2;
+    document.getElementById("r").textContent = resultado;
+}
+
+function multiplicar() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const resultado = num1 * num2;
+    document.getElementById("r").textContent = resultado;
+}
+
+function dividir() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    if (num2 === 0) {
+        document.getElementById("r").textContent = "Error: división entre cero";
+    } else {
+        const resultado = num1 / num2;
+        document.getElementById("r").textContent = resultado;
+    }
+}
